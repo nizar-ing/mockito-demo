384a0291c1f5d449967cae2c4e745d40d8477bdf
nizarIng <nizar.ilahi@gmail.com>
2024-03-02 18:33:17 +0100

    Making unit tests using Mockito annotations.

--- src/test/java/com/nizing/mockitodemo/business/SomeBusinessImplMockTest.java
+++ src/test/java/com/nizing/mockitodemo/business/SomeBusinessImplMockTest.java
@@ -1,28 +1,45 @@
 package com.nizing.mockitodemo.business;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class SomeBusinessImplMockTest {
+    @Mock
+    private DataService dataServiceMock;
+    @InjectMocks
+    private SomeBusinessImpl businessImpl;
+
     @Test
     void findTheGreatestFromAllData() {
-        DataService dataServiceMock = mock(DataService.class);
+        //DataService dataServiceMock = mock(DataService.class);
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{25, 15, 5});
 
-        var businessImpl = new SomeBusinessImpl(dataServiceMock);
+        //var businessImpl = new SomeBusinessImpl(dataServiceMock);
         assertEquals(25, businessImpl.retrieveTheGreatestFromAllData());
     }
 
     @Test
     void findTheGreatestFromAllData_OneValue() {
-        DataService dataServiceMock = mock(DataService.class);
+        //DataService dataServiceMock = mock(DataService.class);
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{35});
 
-        var businessImpl = new SomeBusinessImpl(dataServiceMock);
+        //var businessImpl = new SomeBusinessImpl(dataServiceMock);
         assertEquals(35, businessImpl.retrieveTheGreatestFromAllData());
+    }
+
+    @Test
+    void findTheGreatestFromAllData_EmptyArray() {
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{});
+
+        assertEquals(Integer.MIN_VALUE, businessImpl.retrieveTheGreatestFromAllData());
     }
 }
 
